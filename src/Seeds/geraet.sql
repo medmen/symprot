aaa6da38952d93eac5086485c7da8dbd95a1b7dc
med-men <med-men@gmx.net>
2021-12-25 22:01:00 +0100

    adding sql exports lets save last error log for later fixing

---
+++ src/Seeds/geraet.sql
@@ -0,0 +1,2 @@
+insert into main.geraet (geraet_id, geraet_name, geraet_beschreibung) values (1, 'MRT', '<div>Bei mehreren geräten hilfreich zur Unterscheidung</div>');
+insert into main.geraet (geraet_id, geraet_name, geraet_beschreibung) values (2, 'CT', '<div>Ohne Klappern, blitzeschnell, aber strahlt wie die Sonne</div>');
