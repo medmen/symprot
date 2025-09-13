95d6d32a5b6cf02cf280e7dfe932406c0ec6c962
medmen <med-men@gmx.net>
2025-09-13 16:03:36 +0200

    fixed error with on/off switch for CSP in template

--- src/Twig/CspExtension.php
+++ src/Twig/CspExtension.php
@@ -15,12 +15,14 @@ class CspExtension extends AbstractExtension
     public function getFunctions(): array
     {
         return [
+            // Accept optional named argument "usage" for compatibility with other CSP Twig conventions
             new TwigFunction('csp_nonce', [$this, 'getNonce']),
         ];
     }
 
-    public function getNonce(): string
+    public function getNonce(?string $usage = null): string
     {
+        // Ignore $usage, nonce is the same regardless of usage
         $request = $this->requestStack->getCurrentRequest();
         if (!$request) {
             return '';
